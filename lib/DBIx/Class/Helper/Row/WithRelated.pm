e5b1cae66eb584412611cf26e67ea28975c08dc7
Stefan Adams <stefan@cogentinnovators.com>
2013-03-07 03:07:41 -0600

    Fix problem with insert

--- lib/DBIx/Class/Helper/Row/WithRelated.pm
+++ lib/DBIx/Class/Helper/Row/WithRelated.pm
@@ -9,8 +9,8 @@ sub insert_with_related {
 	$self->$_($row->{me}->{$_}) for keys %{$row->{me}};
 	my $_insert = $self->insert;
 	for my $table ( grep { $_ ne 'me' } keys %$row ) {
-		my $table_rs = $create->search_related($table);
-		$table_rs->$_($row->{$_}) for keys %{$row->{$table}};
+		my $table_rs = $insert->search_related($table)->first;
+		$table_rs->$_($row->{$table}->{$_}) for keys %{$row->{$table}};
 		$table_rs->update;
 	}
 	return $_insert;
@@ -22,8 +22,8 @@ sub update_with_related {
 	$self->$_($row->{me}->{$_}) for keys %{$row->{me}};
 	my $_update = $self->update;
 	for my $table ( grep { $_ ne 'me' } keys %$row ) {
-		my $table_rs = $update->search_related($table);
-		$table_rs->$_($row->{$_}) for keys %{$row->{$table}};
+		my $table_rs = $update->search_related($table)->first;
+		$table_rs->$_($row->{$table}->{$_}) for keys %{$row->{$table}};
 		$table_rs->update;
 	}
 	return $_update;
