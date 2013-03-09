6b26dc94a59ff948732d0fdab384ca7a2f14fa12
Stefan Adams <stefan@cogentinnovators.com>
2013-03-08 23:51:58 -0600

    Allow specifying table name on ResultSet year methods

--- lib/WRRA/Schema/ResultSet.pm
+++ lib/WRRA/Schema/ResultSet.pm
@@ -7,9 +7,10 @@ __PACKAGE__->load_components(qw(Helper::ResultSet::Me Helper::ResultSet::Mojolic
 use strict;
 use warnings;
 
-sub next_year { $_[0]->search({$_[0]->me.'year' => $_[0]->session->{year}+1}) }
-sub current_year { $_[0]->search({$_[0]->me.'year' => $_[0]->session->{year}}) }
-sub last_year { $_[0]->search({$_[0]->me.'year' => $_[0]->session->{year}-1}) }
-sub recent_years { $_[0]->search({$_[0]->me.'year' => {-between => [$_[0]->session->{year}-$_[0]->session->{recent_years}+1, $_[0]->session->{year}]}}) }
+sub year { $_[0]->search({($_[1]?"$_[1].year":$_[0]->me.'year') => $_[2]||$_[0]->session->{year}}) }
+sub next_year { $_[0]->search({($_[1]?"$_[1].year":$_[0]->me.'year') => $_[0]->session->{year}+1}) }
+sub current_year { $_[0]->search({($_[1]?"$_[1].year":$_[0]->me.'year') => $_[0]->session->{year}}) }
+sub last_year { $_[0]->search({($_[1]?"$_[1].year":$_[0]->me.'year') => $_[0]->session->{year}-1}) }
+sub recent_years { $_[0]->search({($_[1]?"$_[1].year":$_[0]->me.'year') => {-between => [$_[0]->session->{year}-$_[0]->session->{recent_years}+1, $_[0]->session->{year}]}}) }
 
 1;
