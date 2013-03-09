c53b5b0c1a7a284e466b2629fc52888d1e735a2b
Stefan Adams <stefan@cogentinnovators.com>
2013-03-08 21:27:44 -0600

    Ad Bellitems

--- lib/WRRA/Schema/Result/Bellitems.pm
+++ lib/WRRA/Schema/Result/Bellitems.pm
@@ -2,11 +2,17 @@ package WRRA::Schema::Result::Bellitems;
 
 use base 'WRRA::Schema::Result::Bellitem';
 
-sub _search {
-        my ($self, $rs, $req) = @_;
-        $rs->current_year;
-}
+sub _colmodel { qw/id name/ }
 
-sub TO_VIEW { qw/id name/ }
+sub _create {
+	my ($class, $r, $rs, $req) = @_;
+	$r->year($rs->session->{year});
+	return $r;
+};
+
+sub _search {
+	my ($self, $rs, $req) = @_;
+	$rs->current_year;
+}
 
 1;
