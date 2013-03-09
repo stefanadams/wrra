d07d0e4e53ce85d86eaa8200055d07caa1b1e511
Stefan Adams <stefan@cogentinnovators.com>
2013-03-08 20:18:06 -0600

    Update Flyer columns

--- lib/WRRA/Schema/Result/Flyer.pm
+++ lib/WRRA/Schema/Result/Flyer.pm
@@ -2,11 +2,11 @@ package WRRA::Schema::Result::Flyer;
 
 use base 'WRRA::Schema::Result::Item';
 
+sub _colmodel { qw/scheduled.day_name name donor.name value/ }
+
 sub _search {
         my ($self, $rs, $req) = @_;
         $rs->search({}, {order_by=>{'-asc'=>['scheduled','number']}})->current_year
 } 
-
-sub TO_VIEW { qw/name value scheduled.day_name/ }
 
 1;
