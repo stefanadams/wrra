73095ff45357c22126f8fde71fbc7430cb91b94c
Stefan Adams <stefan@cogentinnovators.com>
2013-03-10 11:45:03 -0500

    Rewrite Ads to use datetime

--- lib/WRRA/Schema/ResultSet/Adcount.pm
+++ lib/WRRA/Schema/ResultSet/Adcount.pm
@@ -3,4 +3,11 @@ use base 'WRRA::Schema::ResultSet';
 
 sub random { shift->search({}, {order_by=>[{'-asc'=>'rotate'}, \'RAND()']}) }
 
+sub today { my $self = shift; $self->current_year->search({scheduled=>[''.$self->datetime,{'='=>undef}]}) }
+sub yesterday { my $self = shift; $self->current_year->search({scheduled=>[''.$self->datetime,{'='=>undef}]}) }
+sub tomorrow { my $self = shift; $self->current_year->search({scheduled=>[''.$self->datetime,{'='=>undef}]}) }
+sub first_day { my $self = shift; $self->current_year->search({scheduled=>[''.$self->datetime,{'='=>undef}]}) }
+sub last_day { my $self = shift; $self->current_year->search({scheduled=>[''.$self->datetime,{'='=>undef}]}) }
+sub all_days { my $self = shift; $self->current_year->search({scheduled=>[''.$self->datetime,{'='=>undef}]}) }
+
 1;
