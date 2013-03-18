ca7fda8fe473f49c41e4efd6612594aef2030c21
Stefan Adams <stefan@cogentinnovators.com>
2013-03-18 04:24:43 -0500

    Improvements

--- lib/DBIx/Class/Helper/Row/WithRelated.pm
+++ lib/DBIx/Class/Helper/Row/WithRelated.pm
@@ -9,7 +9,7 @@ sub insert_with_related {
 	$self->$_(length($row->{me}->{$_})?$row->{me}->{$_}:undef) for grep { $self->can($_) } keys %{$row->{me}};
 	my $_insert = $self->insert;
 	for my $table ( grep { $_ ne 'me' } keys %$row ) {
-		my $table_rs = $insert->search_related($table)->first;
+		my $table_rs = $insert->search_related($table)->first or next;
 		$table_rs->$_(length($row->{$table}->{$_})?$row->{$table}->{$_}:undef) for grep { $table_rs->can($_) } keys %{$row->{$table}};
 		$table_rs->update;
 	}
@@ -22,7 +22,7 @@ sub update_with_related {
 	$self->$_(length($row->{me}->{$_})?$row->{me}->{$_}:undef) for grep { $self->can($_) } keys %{$row->{me}};
 	my $_update = $self->update;
 	for my $table ( grep { $_ ne 'me' } keys %$row ) {
-		my $table_rs = $update->search_related($table)->first;
+		my $table_rs = $update->search_related($table)->first or next;
 		$table_rs->$_(length($row->{$table}->{$_})?$row->{$table}->{$_}:undef) for grep { $table_rs->can($_) } keys %{$row->{$table}};
 		$table_rs->update;
 	}
