1a966b99f0d41c9b279bea9d83bac8b67fa57023
Stefan Adams <stefan@cogentinnovators.com>
2013-03-18 05:17:20 -0500

    Small fixes

--- lib/WRRA/Schema/Result/Bankreport.pm
+++ lib/WRRA/Schema/Result/Bankreport.pm
@@ -2,11 +2,11 @@ package WRRA::Schema::Result::Bankreport;
 
 use base 'WRRA::Schema::Result::Item';
 
-sub _search {
-        my ($self, $rs, $req) = @_;
-        $rs;
-}
+sub _colmodel { qw/sold.day_name number name highbid.bid highbid.bidder.name value donor.name/ }
 
-sub TO_VIEW { qw/soldday name highbid.bid/ }
+sub _search {
+	my ($self, $rs, $req) = @_;
+	$rs->search({}, {order_by=>['scheduled','number']})->current_year->sold
+}
 
 1;
