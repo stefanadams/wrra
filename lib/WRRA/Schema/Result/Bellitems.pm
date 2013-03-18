63a9c9cc74d4c080dd1bb695c5192b62cddf1c5a
Stefan Adams <stefan@cogentinnovators.com>
2013-03-18 04:19:42 -0500

    Manage Winners and Bellringers

--- lib/WRRA/Schema/Result/Bellitems.pm
+++ lib/WRRA/Schema/Result/Bellitems.pm
@@ -2,17 +2,34 @@ package WRRA::Schema::Result::Bellitems;
 
 use base 'WRRA::Schema::Result::Bellitem';
 
+# The columns to be rendered
 sub _colmodel { qw/id name/ }
 
-sub _create {
-	my ($class, $r, $rs, $req) = @_;
-	$r->year($rs->datetime->year);
-	return $r;
-};
+# The columns to be rendered as special accessor methods
+#sub received { shift->SUPER::received->ymd }
+#sub sold { shift->SUPER::sold ? 1 : 0 }
 
-sub _search {
-	my ($self, $rs, $req) = @_;
-	$rs->current_year;
-}
+# These class methods are passed $rs, $request
+sub _create {
+        my ($class, $r, $rs, $req) = @_;
+        $r->year($rs->datetime->year);
+        return $r;
+};
+sub _search { $_[1]->current_year };
+#sub _update { $_[1] };
+#sub _delete { $_[1] };
+
+# The relationships associated with this result (table)
+#our $relationships = [];
+
+# When reading (i.e. searching or ordering), use this hashref to lookup references (scalar) or use mysql functions (scalarref)
+# As a special case, if an arrayref is provided, index [0] is provided for search routines and index [1] is provided for ordering routines.
+# If not an arrayref then the same handler is used for both search and ordering.
+# if a coderef is provided, pass a hashref of the search condition to search or nothing to ordering.  Expects a fully formulated SQL::Abstract ref.
+#our $read = {};
+# When editing (i.e. creating or updating), use this hashref to lookup coderefs and pass the value to provide and expect back a full hash of key/value pairs.
+#our $edit = {};
+# For each field being edited (i.e. created or updated) validate the value with the qr regex in this hashref
+#our $validate = {};
 
 1;
---
+++ lib/WRRA/Schema/Result/BsBellitems.pm
@@ -0,0 +1,11 @@
+package WRRA::Schema::Result::BsBellitems;
+
+use base 'WRRA::Schema::Result::Bellitem';
+
+sub _colmodel { qw/id name/ }
+
+#sub _search {
+#	my ($self, $rs, $req) = @_;
+#} 
+
+1;
